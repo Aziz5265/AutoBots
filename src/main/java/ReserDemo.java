c9b2f1b04668651877ec0deefc88f434b1a8c22c
Aziz <azzizz5265@gmail.com>
2025-07-15 17:56:29 +0600

    version 1.2

--- src/main/java/ReserDemo.java
+++ src/main/java/ReserDemo.java
@@ -2,5 +2,6 @@ public class ReserDemo {
     public static void main(String[] args) {
         System.out.println("version 1.0");
         System.out.println("version 1.1");
+        System.out.println("version 1.2");
     }
 }
