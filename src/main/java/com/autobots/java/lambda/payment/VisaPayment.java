fd68b00610ed44786190e669ab57cc44e987cd6b
Aziz <azzizz5265@gmail.com>
2025-07-15 14:48:24 +0600

    Visa Card

---
+++ src/main/java/com/autobots/java/lambda/payment/VisaPayment.java
@@ -0,0 +1,11 @@
+package com.autobots.java.lambda.payment;
+
+
+public class VisaPayment {
+
+    public void payByVisaCard (double amount){
+        System.out.println("visa payment by visa card");
+    }
+
+
+}
