f0fffab1dc1006f6e5e3b361a3e35025816f0d1c
Aziz <azzizz5265@gmail.com>
2025-07-15 18:11:36 +0600

    version 1.3 mixed

--- src/main/java/ReserDemo.java
+++ src/main/java/ReserDemo.java
@@ -3,5 +3,7 @@ public class ReserDemo {
         System.out.println("version 1.0");
         System.out.println("version 1.1");
         System.out.println("version 1.2");
+        System.out.println("version 1.3 --> hello world");
+        System.out.println("version 1.3 --> mixed");
     }
 }
